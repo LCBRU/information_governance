4172e37e23f9b0058866300ef6910ee1a2b17de0
Richard Bramley <rabramley@gmail.com>
2020-06-12 13:11:32 +0100

    Show applications

--- app/ui/views/home.py
+++ app/ui/views/home.py
@@ -1,7 +1,17 @@
 from flask import render_template, redirect, url_for
 from .. import blueprint
+from app.model import Application
 
 
 @blueprint.route("/")
 def index():
-    return render_template("index.html")
+    applications = Application.query.order_by(Application.name).all()
+
+    return render_template("index.html", applications=applications)
+
+
+@blueprint.route("/security_statement/<int:id>")
+def security_statement(id):
+    application = Application.query.get_or_404(id)
+
+    return render_template("security_statement.html", application=application)
